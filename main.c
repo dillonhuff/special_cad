11c6250d74c8fca81f7a253cd2718b2fdd5a4418
Dillon Huff <dillonhuff@gmail.com>
2017-08-01 15:40:05 -0700

    Now taking another pass at building multivariate polynomials

--- main.c
+++ main.c
@@ -11,61 +11,64 @@
 
 void isolate_multivariate_roots() {
 
-  /* lp_variable_db_t* var_db = lp_variable_db_new(); */
+  lp_variable_db_t* var_db = lp_variable_db_new();
 
-  /* // Create variables */
-  /* lp_variable_t u = lp_variable_db_new_variable(var_db, "u");   */
+  // Create variables
+  lp_variable_t u = lp_variable_db_new_variable(var_db, "u");
 
-  /* // Create variable order */
-  /* lp_variable_order_t* var_order = lp_variable_order_new(); */
-  /* lp_variable_order_push(var_order, u); */
+  // Create variable order
+  lp_variable_order_t* var_order = lp_variable_order_new();
+  lp_variable_order_push(var_order, u);
 
-  /* lp_polynomial_context_t* ctx = */
-  /*   lp_polynomial_context_new(lp_Z, var_db, var_order); */
+  lp_polynomial_context_t* ctx =
+    lp_polynomial_context_new(lp_Z, var_db, var_order);
   
-  /* printf("Printing a number\n"); */
+  printf("Printing a number\n");
 
-  /* lp_integer_t it; */
-  /* lp_integer_construct_from_int(lp_Z, &it, 23); */
+  lp_integer_t it;
+  lp_integer_construct_from_int(lp_Z, &it, 23);
 
-  /* lp_integer_t one; */
-  /* lp_integer_construct_from_int(lp_Z, &one, 1); */
+  lp_integer_t one;
+  lp_integer_construct_from_int(lp_Z, &one, 1);
   
-  /* lp_integer_print(&it, stdout); */
-  /* printf("\n"); */
+  lp_integer_print(&it, stdout);
+  printf("\n");
 
   // Build up polynomial 23u^2 + u from monomials
-  /* lp_polynomial_t* x2 = lp_polynomial_new(ctx); */
-  /* lp_polynomial_construct_simple(x2, ctx, &it, u, 2); */
+  lp_polynomial_t* x2 = lp_polynomial_new(ctx);
+  lp_polynomial_construct_simple(x2, ctx, &it, u, 2);
 
-  /* lp_polynomial_print(x2, stdout); */
-  /* printf("\n"); */
+  lp_polynomial_print(x2, stdout);
+  printf("\n");
   
-  /* lp_polynomial_t* x = lp_polynomial_new(ctx); */
-  /* lp_polynomial_construct_simple(x, ctx, &one, u, 1); */
+  lp_polynomial_t* x = lp_polynomial_new(ctx);
+  lp_polynomial_construct_simple(x, ctx, &one, u, 1);
 
-  /* lp_polynomial_print(x, stdout); */
-  /* printf("\n"); */
+  lp_polynomial_print(x, stdout);
+  printf("\n");
   
-  /* lp_polynomial_t* poly = lp_polynomial_new(ctx); */
-  /* lp_polynomial_add(poly, x2, x); */
+  lp_polynomial_t* poly = lp_polynomial_new(ctx);
+  lp_polynomial_add(poly, x2, x);
 
-  /* lp_polynomial_print(poly, stdout); */
-  /* printf("\n"); */
+  lp_polynomial_print(poly, stdout);
+  printf("\n");
 
   // Isolate roots
-  //lp_assignment_t* assignment = lp_assignment_new(var_db);
+  lp_assignment_t* assignment = lp_assignment_new(var_db);
 
-  /* void* data = malloc(sizeof(lp_rational_t)*deg); */
-  /* lp_value_t* roots = lp_value_new(LP_VALUE_RATIONAL, data); */
+  size_t deg = lp_polynomial_degree(poly);
 
-  //lp_polynomial_roots_isolate(poly, assignment, roots, &roots_size);
+  void* data = malloc(sizeof(lp_rational_t)*deg);
+  lp_value_t* roots = lp_value_new(LP_VALUE_RATIONAL, data);
+
+  size_t roots_size;
+  lp_polynomial_roots_isolate(poly, assignment, roots, &roots_size);
 
   // Cleanup
-  /* lp_assignment_delete(assignment); */
-  /* lp_polynomial_context_detach(ctx); */
-  /* lp_variable_db_detach(var_db); */
-  /* lp_variable_order_detach(var_order); */
+  lp_assignment_delete(assignment);
+  lp_polynomial_context_detach(ctx);
+  lp_variable_db_detach(var_db);
+  lp_variable_order_detach(var_order);
 
 }
 
