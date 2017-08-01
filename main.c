f0a3a09abd8a881d95f93a4fabb34bbf1b76dd28
Dillon Huff <dillonhuff@gmail.com>
2017-08-01 14:45:50 -0700

    Can now isolate roots of univariate polynomial

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#include <poly/assignment.h>
 #include <poly/polynomial.h>
 #include <poly/variable_order.h>
 #include <poly/variable_db.h>
@@ -22,12 +24,29 @@ int main() {
   printf("\n");
 
   lp_polynomial_t* poly = lp_polynomial_new(ctx);
-  lp_polynomial_construct_simple(poly, ctx, &it, 1, 1);
+  lp_polynomial_construct_simple(poly, ctx, &it, 1, 2);
 
   lp_polynomial_print(poly, stdout);
   printf("\n");
 
+  size_t deg = lp_polynomial_degree(poly);
+
+  printf("Degree of poly = %zu\n", deg);
+
+  lp_assignment_t* assignment = lp_assignment_new(var_db);
+
+  void* data = malloc(sizeof(lp_algebraic_number_t)*deg);
+  lp_value_t* roots = lp_value_new(LP_VALUE_ALGEBRAIC, data);
+
+  size_t roots_size;
+  lp_polynomial_roots_isolate(poly, assignment, roots, &roots_size);
+
+  printf("# of roots of poly = %zu\n", roots_size);
+
+  // Cleanup
   lp_polynomial_context_detach(ctx);
   lp_variable_db_detach(var_db);
   lp_variable_order_detach(var_order);
+
+
 }
