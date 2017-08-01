284dd6c0aa1b9171d6aec6ec386d1737a76188fd
Dillon Huff <dillonhuff@gmail.com>
2017-08-01 15:34:01 -0700

    Separating out univariate and multivariate root isolation

--- main.c
+++ main.c
@@ -9,29 +9,30 @@
 #include <poly/upolynomial.h>
 #include <poly/poly.h>
 
-void isolate_univariate_roots() {
-  lp_variable_db_t* var_db = lp_variable_db_new();
+void isolate_multivariate_roots() {
 
-  // Create variables
-  lp_variable_t u = lp_variable_db_new_variable(var_db, "u");  
+  /* lp_variable_db_t* var_db = lp_variable_db_new(); */
 
-  // Create variable order
-  lp_variable_order_t* var_order = lp_variable_order_new();
-  lp_variable_order_push(var_order, u);
+  /* // Create variables */
+  /* lp_variable_t u = lp_variable_db_new_variable(var_db, "u");   */
 
-  lp_polynomial_context_t* ctx =
-    lp_polynomial_context_new(lp_Z, var_db, var_order);
+  /* // Create variable order */
+  /* lp_variable_order_t* var_order = lp_variable_order_new(); */
+  /* lp_variable_order_push(var_order, u); */
+
+  /* lp_polynomial_context_t* ctx = */
+  /*   lp_polynomial_context_new(lp_Z, var_db, var_order); */
   
-  printf("Printing a number\n");
+  /* printf("Printing a number\n"); */
 
-  lp_integer_t it;
-  lp_integer_construct_from_int(lp_Z, &it, 23);
+  /* lp_integer_t it; */
+  /* lp_integer_construct_from_int(lp_Z, &it, 23); */
 
-  lp_integer_t one;
-  lp_integer_construct_from_int(lp_Z, &one, 1);
+  /* lp_integer_t one; */
+  /* lp_integer_construct_from_int(lp_Z, &one, 1); */
   
-  lp_integer_print(&it, stdout);
-  printf("\n");
+  /* lp_integer_print(&it, stdout); */
+  /* printf("\n"); */
 
   // Build up polynomial 23u^2 + u from monomials
   /* lp_polynomial_t* x2 = lp_polynomial_new(ctx); */
@@ -52,6 +53,24 @@ void isolate_univariate_roots() {
   /* lp_polynomial_print(poly, stdout); */
   /* printf("\n"); */
 
+  // Isolate roots
+  //lp_assignment_t* assignment = lp_assignment_new(var_db);
+
+  /* void* data = malloc(sizeof(lp_rational_t)*deg); */
+  /* lp_value_t* roots = lp_value_new(LP_VALUE_RATIONAL, data); */
+
+  //lp_polynomial_roots_isolate(poly, assignment, roots, &roots_size);
+
+  // Cleanup
+  /* lp_assignment_delete(assignment); */
+  /* lp_polynomial_context_detach(ctx); */
+  /* lp_variable_db_detach(var_db); */
+  /* lp_variable_order_detach(var_order); */
+
+}
+
+void isolate_univariate_roots() {
+
   lp_upolynomial_t* x2 = lp_upolynomial_construct_power(lp_Z, 2, 23);
   lp_upolynomial_t* x = lp_upolynomial_construct_power(lp_Z, 1, 1);
 
@@ -63,17 +82,10 @@ void isolate_univariate_roots() {
   lp_upolynomial_print(poly, stdout);
   printf("\n");
 
-  // Isolate roots
-  lp_assignment_t* assignment = lp_assignment_new(var_db);
-
-  /* void* data = malloc(sizeof(lp_rational_t)*deg); */
-  /* lp_value_t* roots = lp_value_new(LP_VALUE_RATIONAL, data); */
-
   lp_algebraic_number_t* roots =
     (lp_algebraic_number_t*)(malloc(sizeof(lp_algebraic_number_t)*deg));
 
   size_t roots_size;
-  //lp_polynomial_roots_isolate(poly, assignment, roots, &roots_size);
   lp_upolynomial_roots_isolate(poly, roots, &roots_size);
 
   printf("# of roots of poly = %zu\n", roots_size);
@@ -82,16 +94,7 @@ void isolate_univariate_roots() {
     printf("ROOT = ");
     lp_algebraic_number_print(&(roots[i]), stdout);
     printf("\n");
-    //printf("root type = %d\n", roots[i].type);
-    //printf("root approximation = %f\n", lp_value_to_double(&(roots[i])));
   }
-
-  // Cleanup
-  
-  lp_assignment_delete(assignment);
-  lp_polynomial_context_detach(ctx);
-  lp_variable_db_detach(var_db);
-  lp_variable_order_detach(var_order);
 
 }
 
