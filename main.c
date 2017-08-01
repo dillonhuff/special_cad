c071e8b91a50da5fc05008660cf3a827054356c0
Dillon Huff <dillonhuff@gmail.com>
2017-08-01 12:41:48 -0700

    Now have code compiling and running without any major errors

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-/* #include <poly/variable_db.h> */
-/* #include <poly/poly.h> */
-#include <poly/polynomial.h>
+#include <poly/variable_order.h>
+#include <poly/variable_db.h>
+#include <poly/polynomial_context.h>
+#include <poly/poly.h>
+
+
 
 int main() {
-  //lp_variable_db_t var_db;
-  /* lp_variable_order_t var_order; */
+  lp_variable_db_t* var_db = lp_variable_db_new();
+  lp_variable_order_t* var_order = lp_variable_order_new();
 
-  lp_polynomial_context_t ctx; //* ctx =
-  //lp_polynomial_context_new(lp_Z, &var_db, var_order);
+  lp_polynomial_context_t* ctx =
+    lp_polynomial_context_new(lp_Z, var_db, var_order);
   
   printf("Printing\n");
 
-  lp_polynomial_context_detach(&ctx);
+  lp_polynomial_context_detach(ctx);
+  lp_variable_db_detach(var_db);
 }
