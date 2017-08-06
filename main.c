75597dfdcc0340bdb04dea38c25b3e8fb64b12cd
Dillon Huff <dillonhuff@gmail.com>
2017-08-06 13:16:49 -0700

    Can now build the the first algebraic number root

--- main.c
+++ main.c
@@ -984,6 +984,45 @@ lp_value_t* dyadic_rational_normalization_error_roots() {
   lp_value_t* roots =
     (lp_value_t*) malloc(sizeof(lp_value_t)*3);
 
+  lp_rational_t five_halves;
+  lp_rational_construct_from_int(&five_halves, 5, 2);
+  lp_value_construct(&(roots[0]), LP_VALUE_RATIONAL, &five_halves);
+
+  //root[1] = <3*x^2 + (-1*x) + (-1), (-445/1024, -889/2048)>
+  //root[2] = <61*x^2 + 8*x + (-8), (-889/2048, -111/256)>
+
+  lp_upolynomial_t* x2 = lp_upolynomial_construct_power(lp_Z, 2, 3);
+  lp_upolynomial_t* mx = lp_upolynomial_construct_power(lp_Z, 1, -1);
+  lp_upolynomial_t* mone = lp_upolynomial_construct_power(lp_Z, 0, -1);
+
+  lp_upolynomial_t* poly1 = lp_upolynomial_add(x2, mx);
+  lp_upolynomial_t* poly = lp_upolynomial_add(poly1, mone);
+  size_t deg = lp_upolynomial_degree(poly);
+
+  lp_upolynomial_print(poly, stdout);
+  printf("\n");
+
+  lp_dyadic_rational_t num;
+  lp_dyadic_rational_construct_from_int(&num, -445, 10);
+
+  lp_dyadic_rational_t denum;
+  lp_dyadic_rational_construct_from_int(&denum, -889, 11);
+
+  lp_dyadic_interval_t it;
+  lp_dyadic_interval_construct(&it, &num, 1, &denum, 1);
+
+  lp_dyadic_interval_print(&it, stdout);
+  printf("\n");
+
+  lp_algebraic_number_t algnum_1;
+  lp_algebraic_number_construct(&algnum_1, poly, &it);
+  lp_value_construct(&(roots[1]), LP_VALUE_ALGEBRAIC, &algnum_1);
+
+  
+  /* lp_algebraic_number_t algnum_2; */
+  /* lp_algebraic_number_construct(&algnum_2, poly, &it); */
+  /* lp_value_construct(&(roots[2]), LP_VALUE_ALGEBRAIC, &algnum_2); */
+  
   return roots;
 }
 
@@ -1054,24 +1093,26 @@ void test_constant_conic_sections_unlifted() {
 
   lp_assignment_t* asg = lp_assignment_new(var_db);
 
-  //size_t num_roots = 0;
+  /* size_t num_roots = 0; */
+  /* lp_value_t* all_roots = //dyadic_rational_normalization_error_roots(); */
+  /*   all_sorted_roots(&num_roots, asg, mc_proj1, projection_set_size); */
+
   size_t num_roots = 3;
   lp_value_t* all_roots = dyadic_rational_normalization_error_roots();
-    //all_sorted_roots(&num_roots, asg, mc_proj1, projection_set_size);
 
   //all_roots = build_dyadic_normalization_error_roots();
 
   printf("# of roots = %zu\n", num_roots);
 
-  printf("root[0] = ");
+  printf("root[0] with type %u = ", all_roots[0].type);
   lp_value_print(&(all_roots[0]), stdout);
   printf("\n");
 
-  printf("root[1] = ");
+  printf("root[1] with type %u = ", all_roots[1].type);
   lp_value_print(&(all_roots[1]), stdout);
   printf("\n");
 
-  printf("root[2] = ");
+  printf("root[2] with type %u = ", all_roots[2].type);
   lp_value_print(&(all_roots[2]), stdout);
   printf("\n");
   
